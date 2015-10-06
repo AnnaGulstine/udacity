900f5a984e0ff9ad84338a9c3f192aa26d78ba62
EMILY EKDAHL <emily.ekdahl@gmail.com>
2015-10-05 20:40:02 -0500

    installed grunt on my performance project

---
+++ perf/Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+	  uglify: {
+	    my_target: {
+	      files: {
+	        'views/js/main.min.js': ['views/js/main.js']
+	      }
+	    }
+	  }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	grunt.registerTask('default', ['dev']);
+	grunt.registerTask('dev', ['uglify']);
+};
